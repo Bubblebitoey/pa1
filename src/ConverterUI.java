b1b72d4cf12f4f78cccf717358e5878c377f5d70
Bubblebitoey <bubblebitoey@Bubblebitoeys-MacBook-Pro.local>
2016-04-22 17:56:47 +0700

    can convert

--- src/ConverterUI.java
+++ src/ConverterUI.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import javax.swing.text.AbstractDocument.Content;
 
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,51 +14,56 @@ public class ConverterUI extends JFrame {
 	private JComboBox<Unit> secondUnit;
 	private JLabel equalSign = new JLabel("=");
 	private JButton clearButton = new JButton("Clear");
-	private UnitConverter converter;
-	
+
 	public void run() {
 		setVisible(true);
 	}
-	
+
 	private void initComponents() {
-		
+
 		FlowLayout layout = new FlowLayout();
 		this.setLayout(layout);
-		
-		firstInput = new JTextField("                             ");  
+
+		firstInput = new JTextField("                             ");
 		secondInput = new JTextField("                            ");
 		unitconverter = new UnitConverter();
-		
-		
-		
-		converter = new UnitConverter();
-		
+
 		firstUnit = new JComboBox<Unit>();
 		Unit[] lengths = unitconverter.getUnits();
-		for(Unit u : lengths ) firstUnit.addItem(u);
+		for (Unit u : lengths)
+			firstUnit.addItem(u);
 		firstUnit = new JComboBox<Unit>(lengths);
-		
+
 		secondUnit = new JComboBox<Unit>();
 		Unit[] lengths1 = unitconverter.getUnits();
-		for(Unit u : lengths ) secondUnit.addItem(u);
+		for (Unit u : lengths)
+			secondUnit.addItem(u);
 		secondUnit = new JComboBox<Unit>(lengths1);
 
-	
+		ActionListener listener = new ConvertButtonListener();
+
+		convertButton.addActionListener(listener);
+		clearButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
 		this.add(firstInput);
 		this.add(firstUnit);
 		this.add(equalSign);
 		this.add(secondInput);
 		this.add(secondUnit);
-		this.add( convertButton );
+		this.add(convertButton);
 		this.add(clearButton);
-		
-		this.pack();
-		
-		
-	}	
 
-	
-	public ConverterUI (UnitConverter uc) {
+		this.pack();
+	}
+
+	public ConverterUI(UnitConverter uc) {
 		this.unitconverter = uc;
 		this.setTitle("Length Converter");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -70,17 +74,23 @@ public class ConverterUI extends JFrame {
 
 		@Override
 		public void actionPerformed(ActionEvent evt) {
-//			String s = firstInput.getText().trim();
-//			
-//			System.out.println("actionPerformed: input=" + s);
-//			
-//			if (s.length() > 0) {
-//				
-//				Double recieveWord = Double.valueOf(s);
-//				
-//			}
-//			
+			String s = firstInput.getText().trim();
+
+			System.out.println("actionPerformed: input=" + s);
+
+			if (s.length() > 0) {
+				try {
+					Double recieveWord = Double.valueOf(s);
+					Unit unit1 = (Unit) firstUnit.getSelectedItem();
+					Unit unit2 = (Unit) secondUnit.getSelectedItem();
+
+					secondInput.setText(unitconverter.convert(recieveWord, unit1, unit2) + "");
+
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+			}
+
 		}
-		
 	}
 }
